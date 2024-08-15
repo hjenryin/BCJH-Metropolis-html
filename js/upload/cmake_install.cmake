eff3a0525079241dbdf81e512d8bde23c2684a10
hjenryin <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 00:33:57 +0000

    . (From hjenryin/hjenryin/BCJH-Metropolis@0253f0dabf532903f5e2dae59b302b772630a546)

--- js/upload/cmake_install.cmake
+++ js/upload/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/runner/work/_temp/59a98d5b-d8a5-4f00-b060-f4339a049eef/emsdk-main/upstream/emscripten/cache/sysroot")
+  set(CMAKE_INSTALL_PREFIX "/home/runner/work/_temp/2384a8ec-aeae-482e-a854-5778353a8f6d/emsdk-main/upstream/emscripten/cache/sysroot")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
