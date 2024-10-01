ae4d8c641191b9ce23c580b4abf8e18921860199
hjenryin <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 00:43:37 +0000

    . (From hjenryin/hjenryin/BCJH-Metropolis@117027e525e11b40883a297e828d38f3a631987d)

--- js/upload/bcjh_1.js
+++ js/upload/bcjh_1.js
@@ -16,7 +16,7 @@ var Module = typeof Module != 'undefined' ? Module : {};
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmphwt0f6sm.js
+// include: /tmp/tmpfxhgmxvo.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -194,21 +194,21 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
   })();
 
-// end include: /tmp/tmphwt0f6sm.js
-// include: /tmp/tmpo5eg05ku.js
+// end include: /tmp/tmpfxhgmxvo.js
+// include: /tmp/tmpzr4uo75e.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['ENVIRONMENT_IS_PTHREAD'] || Module['$ww']) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpo5eg05ku.js
-// include: /tmp/tmpbaeodceg.js
+  // end include: /tmp/tmpzr4uo75e.js
+// include: /tmp/tmp5afpwpaz.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpbaeodceg.js
+  // end include: /tmp/tmp5afpwpaz.js
 
 
 // Sometimes an existing Module object exists with properties
--- js/upload/cmake_install.cmake
+++ js/upload/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/runner/work/_temp/6d6ded55-4c71-4a9c-b92a-b6a1244620f6/emsdk-main/upstream/emscripten/cache/sysroot")
+  set(CMAKE_INSTALL_PREFIX "/home/runner/work/_temp/ad45993e-c948-43b6-8811-24611e41ca02/emsdk-main/upstream/emscripten/cache/sysroot")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
