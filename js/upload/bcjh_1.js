7ae143481e08c62eca774713648bcac3656a9c51
hjenryin <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-25 00:43:10 +0000

    . (From hjenryin/hjenryin/BCJH-Metropolis@8162a2a7f782f2c2b1141eaef092eebb9257ac34)

--- js/upload/bcjh_1.js
+++ js/upload/bcjh_1.js
@@ -16,7 +16,7 @@ var Module = typeof Module != 'undefined' ? Module : {};
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp4l5g2d64.js
+// include: /tmp/tmpxcmsom7a.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -194,21 +194,21 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
   })();
 
-// end include: /tmp/tmp4l5g2d64.js
-// include: /tmp/tmpmqbnpv0h.js
+// end include: /tmp/tmpxcmsom7a.js
+// include: /tmp/tmpbo8zal9s.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['ENVIRONMENT_IS_PTHREAD'] || Module['$ww']) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpmqbnpv0h.js
-// include: /tmp/tmpck47il1p.js
+  // end include: /tmp/tmpbo8zal9s.js
+// include: /tmp/tmpb1rlybn0.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpck47il1p.js
+  // end include: /tmp/tmpb1rlybn0.js
 
 
 // Sometimes an existing Module object exists with properties
