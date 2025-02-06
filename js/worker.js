dc06ed82886a389ff379b2c4f98b68e4a8722cd3
hjenryin <72179072+hjenryin@users.noreply.github.com>
2025-02-07 02:02:48 +0800

    新接口+数量适配

--- js/worker.js
+++ js/worker.js
@@ -1,11 +1,17 @@
-self.addEventListener('message', e => {
-  group = JSON.parse(e.data.rule).group;
+self.addEventListener('message', async e => {
+  const response = await fetch('./js_rule_template.json')
+  const ruleTemplate = await response.text()
+
+  const usrd = await fetch('./directUserData.json')
+  const directUserData = await usrd.text()
+
+  group = JSON.parse(ruleTemplate).group;
   importScripts(`./bcjh.js`);
   const progressCall = (progress) => {
       postMessage(progress);
   }
   Module.onRuntimeInitialized = function () {
-    let result = Module.run(e.data.data, e.data.rule, e.data.passline, e.data.iterChef, e.data.iterRep, e.data.allowTool, e.data.recoverStr, progressCall);
+    let result = Module.run(directUserData, ruleTemplate, e.data.passline, e.data.iterChef, e.data.iterRep, e.data.allowTool, e.data.recoverStr, progressCall);
     postMessage(result);
     self.close();
   }
